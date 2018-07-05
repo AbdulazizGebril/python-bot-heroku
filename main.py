e2a5260daccd8088b7916d53c465ba8d3d320136
jared nielsen <nielsen.jared@gmail.com>
2018-07-05 19:20:41 -0400

    more testing

--- main.py
+++ main.py
@@ -10,22 +10,18 @@ import tweepy
 import time
 from datetime import datetime
 import json
-from config import (CONSUMER_KEY,
-                    CONSUMER_SECRET,
-                    ACCESS_TOKEN,
-                    ACCESS_TOKEN_SECRET)
+
 
 # In[2]:
-consumer_key = CONSUMER_KEY
-consumer_secret = CONSUMER_SECRET
-access_token = ACCESS_TOKEN
-access_token_secret = ACCESS_TOKEN_SECRET
+
 
 # Twitter API Keys
-is_prod = os.environ.get('IS_HEROKU', None)
-print(is_prod)
-
-if is_prod:
+try:
+    from config import (consumer_key, 
+                        consumer_secret,
+                        access_token,
+                        access_token_secret)
+except:
     consumer_key = os.environ['CONSUMER_KEY']
     consumer_secret = os.environ['CONSUMER_SECRET']
     access_token = os.environ['ACCESS_TOKEN']
