59f6ca488c4834c8e651526832bd52eae4450922
jared nielsen <nielsen.jared@gmail.com>
2018-07-05 13:21:58 -0400

    rm config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-consumer_key = "ClEop7BvyqdVxPBVLXSf8wHgB"
-consumer_secret = "zenmJeWfpWAH424NVzT4ZLv4Q0dSDtl7AuFAUonCFPHUkJ39L7"
-access_token = "1620574753-VjHYwso6ciFyf17V16eqOncYdmRmEZ12rZoGBAY"
-access_token_secret = "1pXRYcF6vpLPWeBP3aLi0WkazqB6kh9sqlMRJzsjUDXlV"
